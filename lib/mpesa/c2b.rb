973a0a17e6c66d9ddf618b582461c043984df1a3
King'ori Maina <j@kingori.co>
2016-07-16 21:13:02 +0200

    Specify C2B specification code for programmatic access

--- lib/mpesa/c2b.rb
+++ lib/mpesa/c2b.rb
@@ -1,1 +1,7 @@
 require 'mpesa/c2b/client'
+
+module MPesa
+  module C2B
+    VERSION = '2.0'.freeze
+  end
+end
--- spec/c2b/client_spec.rb
+++ spec/c2b/client_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 module MPesa
   module C2B
     describe Client do
+      describe '.VERSION' do
+        it 'has the specification version defined' do
+          expect(MPesa::C2B::VERSION).to eq '2.0'
+        end
+      end
+
       describe '.new' do
         it 'returns client instance' do
           client = MPesa::C2B::Client.new
