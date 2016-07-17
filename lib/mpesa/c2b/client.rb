72808655029a518fd3a65ad12e203164c22626c8
King'ori Maina <j@kingori.co>
2016-07-17 02:08:51 +0200

    Add documentation for MPesa::C2B::Client

--- lib/mpesa/c2b/client.rb
+++ lib/mpesa/c2b/client.rb
@@ -1,6 +1,8 @@
 module MPesa
   module C2B
+    # Client interface for {MPesa::C2B}
     class Client
+      # Creates a new instance of {MPesa::C2B::Client}.
       def initialize
         @client = Savon.client do
           wsdl "#{File.dirname(__FILE__)}/wsdl.xml"
