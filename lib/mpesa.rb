7d09e8976715de2d69db572f3be38215582b997b
King'ori Maina <j@kingori.co>
2016-07-17 22:54:49 +0200

    Add top-level MPesa module documentation

--- lib/mpesa.rb
+++ lib/mpesa.rb
@@ -3,6 +3,17 @@ require 'savon'
 require 'mpesa/c2b'
 require 'mpesa/version'
 
+# Abstaction of the M-Pesa G2 Platform which is a SOAP API (yuck).
+#
+# The new platform provides functionality that covers 3 payment journeys:
+#
+# 1. B2C (Business to Customers)
+# 2. C2B (Customer to Business)
+# 3. B2B (Business to Business)
+#
+# Unlike the old system (G1) where a lot of workarounds had been done to
+# automate payment experiences, developers are now able hook directly to the
+# core M-PESA and get creative.
 module MPesa
   # Get the absolute path of the gem root.
   #
